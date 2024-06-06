03de6d779f3f03956d3b333e37046f28135f7464
giannisaladino <saladinogianni6@gmail.com>
2024-06-06 11:05:07 +0200

    EDIT: edit cards to table at index.blade

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <section>
-    <div class="container">
-    </div>
     <div class="container mt-5">
-        <div class="row g-2">
-            @foreach ($projects as $project )
-                <div class="col-4">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h3>{{ $project->name }}</h3>
-                            <a href="{{ route('admin.projects.show', $project) }}">Leggi</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Slug</th>
+                    <th>Date</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project )
+                <tr>
+                    <th>{{ $project->id }}</th>
+                    <td><a href="{{ route('admin.projects.show', $project) }}">{{ $project->name }}</a></td>
+                    <td>{{ $project->slug }}</td>
+                    <td>{{ $project->date }}</td>
+                    <td><button class="btn btn-primary">EDIT</button></td>
+                    <td><button class="btn btn-danger" >DELETE</button></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </section>
 @endsection
