550f1f836d7b00715b5ff042b333516d2188ac14
giannisaladino <saladinogianni6@gmail.com>
2024-06-06 12:41:53 +0200

    ADD: added type's name to index table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -20,7 +20,7 @@
                 <tr>
                     <th>{{ $project->id }}</th>
                     <td><a href="{{ route('admin.projects.show', $project) }}">{{ $project->name }}</a></td>
-                    <td class="text-center">{{ $project->type_id }}</td>
+                    <td>{{ $project->type->name}}</td>
                     <td>{{ $project->slug }}</td>
                     <td>{{ $project->date }}</td>
                     <td><a class="btn btn-primary" href="">EDIT</a></td>
